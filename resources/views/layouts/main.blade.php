c79e81d3b12ec53cb34a14ecb029398e1095097f
3neef <hajiki.326@hotmail.com>
2020-06-25 06:04:45 +0300

    search dropdown / livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,7 @@
     <title>My Movies</title>
 
     <link rel="stylesheet" href="/css/app.css">
+    <livewire:styles>
 </head>
 <body class="font-sans bg-teal-900 text-white">
     <nav class="border-b border-teal-800">
@@ -28,12 +29,7 @@
                 </li>
             </ul>
             <div class="flex flex-col md:flex-row items-center">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text" class="bg-teal-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
-                    <div class="absolute top-0">
-                        <svg class="fill-current w-4 text-teal-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
-                    </div>
-                </div>
+               <livewire:search-dropdown>
                 <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
                         <img src="/img/miz.jpg" alt="avatar" class="rounded-full w-8 h-8">
@@ -45,5 +41,6 @@
 
 
     @yield('content')
+    <livewire:scripts>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,24 @@ module.exports = {
         '96':'24rem'
       }
     },
+    spinner: (theme) => ({
+      default: {
+        color: '#dae1e7', // color you want to make the spinner
+        size: '1em', // size of the spinner (used for both width and height)
+        border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
+        speed: '500ms', // the speed at which the spinner should rotate
+      },
+      // md: {
+      //   color: theme('colors.red.500', 'red'),
+      //   size: '2em',
+      //   border: '2px',
+      //   speed: '500ms',
+      // },
+    }),
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/custom-forms')
+    require('@tailwindcss/custom-forms'),
+    require('tailwindcss-spinner')(),
   ]
 }
